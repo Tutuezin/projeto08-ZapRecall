2e2f462141786fa69a12343a7d39057182b85d73
Tutuezin <vinnycamara1@gmail.com>
2022-05-13 01:41:25 -0300

    :sparkles: feat: finished question

--- src/pages/Questions/components/Container/index.js
+++ src/pages/Questions/components/Container/index.js
@@ -2,9 +2,15 @@ import { useState } from "react";
 import "./styles.css";
 import vector from "../../../../assets/img/vector.svg";
 import turn from "../../../../assets/img/turn.svg";
+import correct from "../../../../assets/img/correct.svg";
+import soso from "../../../../assets/img/soso.svg";
+import wrong from "../../../../assets/img/wrong.svg";
 
 function Question({ title, alt, question }) {
   const [classQuestion, setClassQuestion] = useState("closedQuestion");
+  const [turned, setTurned] = useState(false);
+  const [finished, setFinished] = useState("");
+  const [classTitle, setClassTitle] = useState("");
 
   const ToggleQuestion = () => {
     setClassQuestion(
@@ -12,15 +18,76 @@ function Question({ title, alt, question }) {
     );
   };
 
-  return classQuestion === "closedQuestion" ? (
+  const TurnQuestion = () => {
+    setTurned(!turned);
+  };
+
+  const FinishedQuestion = () => {
+    switch (finished) {
+      case "":
+        return classQuestion === "closedQuestion" ? vector : turn;
+      case "wrong":
+        return wrong;
+      case "soso":
+        return soso;
+      case "correct":
+        return correct;
+    }
+  };
+  const AfterFinished = () => {
+    if (finished) return () => {};
+    return classQuestion === "closedQuestion"
+      ? ToggleQuestion()
+      : TurnQuestion();
+  };
+
+  return (
     <div className={classQuestion}>
-      <h3>{title}</h3>
-      <img onClick={ToggleQuestion} src={vector} alt={alt} />
-    </div>
-  ) : (
-    <div className={classQuestion}>
-      <h3>{question}</h3>
-      <img width={30} height={20} src={turn} alt="turn" />
+      {!turned ? (
+        <>
+          <h3 className={finished ? "through " + classTitle : ""}>
+            {classQuestion === "closedQuestion" ? title : question}
+          </h3>
+          <img
+            onClick={AfterFinished}
+            src={FinishedQuestion()}
+            alt={classQuestion === "closedQuestion" ? alt : turn}
+          />
+        </>
+      ) : (
+        <div className="zap">
+          <h4
+            onClick={() => {
+              setClassTitle("wrong");
+              setFinished("wrong");
+              setClassQuestion("closedQuestion finished");
+              TurnQuestion();
+            }}
+          >
+            Não lembrei
+          </h4>
+          <h4
+            onClick={() => {
+              setClassTitle("soso");
+              setFinished("soso");
+              setClassQuestion("closedQuestion finished");
+              TurnQuestion();
+            }}
+          >
+            Quase não lembrei
+          </h4>
+          <h4
+            onClick={() => {
+              setClassTitle("correct");
+              setFinished("correct");
+              setClassQuestion("closedQuestion finished");
+              TurnQuestion();
+            }}
+          >
+            Zap!
+          </h4>
+        </div>
+      )}
     </div>
   );
 }
@@ -35,14 +102,42 @@ export default function Container() {
     {
       title: "Pergunta 2",
       alt: "Pergunta 2",
+      question: "O React é __",
     },
     {
       title: "Pergunta 3",
       alt: "Pergunta 3",
+      question: "Componentes devem iniciar com __",
     },
     {
       title: "Pergunta 4",
       alt: "Pergunta 4",
+      question: "Podemos colocar __ dentro do JSX",
+    },
+    {
+      title: "Pergunta 4",
+      alt: "Pergunta 4",
+      question: "Podemos colocar __ dentro do JSX",
+    },
+    {
+      title: "Pergunta 4",
+      alt: "Pergunta 4",
+      question: "Podemos colocar __ dentro do JSX",
+    },
+    {
+      title: "Pergunta 4",
+      alt: "Pergunta 4",
+      question: "Podemos colocar __ dentro do JSX",
+    },
+    {
+      title: "Pergunta 4",
+      alt: "Pergunta 4",
+      question: "Podemos colocar __ dentro do JSX",
+    },
+    {
+      title: "Pergunta 4",
+      alt: "Pergunta 4",
+      question: "Podemos colocar __ dentro do JSX",
     },
   ];
 
